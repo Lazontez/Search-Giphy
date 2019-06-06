58aa2be13a31e585c8e5480d0f8ef064da211523
Tez, Gardner <tezgardner1212@gmail.com>
2019-06-05 20:16:10 -0500

    Added ajax call and response

--- javascript/app.js
+++ javascript/app.js
@@ -31,65 +31,127 @@ window.onload = function () {
     $("#submitBtn").on("click", getUsersInput);
     $("#submitBtn").on("click", usersInputAction);
 
-}
-
-function getUsersInput() {
-    //makes a variable called user-input with the value of the input box
-    usersInput = $("#user-input").val().trim();
-    //clear the input box
-    // clearInputBox()    
-    //adds the user input variable to the topics array
-    topics.push(usersInput);
 
 
-}
-function clearInputBox() {
-    document.getElementById('userData').value = ("");
-
-}
-function usersInputAction() {
-    // Creating and storing an button tag
-    topicBtn = $("<button>");
-    //Adds a attribute to the topic button with a unique number depending on its index
-    topicBtn.attr("data-number", x);
-    topicBtn.attr("class", "btn btn-dark");
-    //Adds the text of the button as the topic
-    topicBtn.append(usersInput)
-    $("#buttonsSaved").prepend(topicBtn)
-
-}
+    function getUsersInput() {
+        //makes a variable called user-input with the value of the input box
+        usersInput = $("#user-input").val().trim();
+        
+        //clear the input box
+        // clearInputBox()    
+        //adds the user input variable to the topics array
+        topics.push(usersInput);
 
 
-$("#submitBtn").click(function () {
-    usersInput = $("#user-input").val().trim();
+    }
+    function clearInputBox() {
+        document.getElementById('userData').value = ("");
+
+    }
+    function usersInputAction() {
+        // Creating and storing an button tag
+        topicBtn = $("<button>");
+        //Adds a attribute to the topic button with a unique number depending on its index
+        topicBtn.attr("data-number", x);
+        topicBtn.attr("class", "btn btn-dark");
+
+        //Adds the text of the button as the topic
+        topicBtn.append(usersInput)
+        $("#buttonsSaved").prepend(topicBtn)
+
+    }
 
 
-    var api_key = "yJvVChLhvqDOJQMutnp3D7JxsX8iaN0N"
+    $("#submitBtn").click(function () {
 
-    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=" + api_key + "&q=" + usersInput + "&limit=10&offset=0&rating=R&lang=en";
+        var api_key = "yJvVChLhvqDOJQMutnp3D7JxsX8iaN0N"
+
+        usersInput = $("#user-input").val().trim();
 
 
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function (response) {
-        console.log(response);
-        var image= response.data.fixed_height_still;
-        console.log(image)
-    //Creating and storing an image tag
-    //      gifImage = $("<img>");
+        var imageRepo = []
+        var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=" + api_key + "&q=" + usersInput + "&limit=10&offset=0&rating=R&lang=en";
+        // console.log(queryURL)
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
 
-    //      // Setting the catImage src attribute to imageUrl
-    //      gifImage.attr("src", imageUrl);
-    //      gifImage.attr("alt", "gifImage---"+userInput);
+            for (var x = 0; x < response.data.length; x++) {
+                var imageStill = response.data[x].images.fixed_height_still.url
+                var animatedImage = response.data[x].images.fixed_height.url;
+                var rating = response.data[x].rating;
+                console.log(rating)
+                // Creating and storing an image tag
+                gifImage = $("<img>");
 
-    //      // Prepending the catImage to the images div
-    //      $("#gifDisplay").prepend(gifImage);
-    //    });
+                // Setting the catImage src attribute to imageUrl
+                gifImage.attr("src", imageStill);
+                gifImage.attr("data-animate", animatedImage);
+                gifImage.attr("data-still", imageStill);
+                gifImage.attr("alt", "gifImage-" + usersInput);
+                gifImage.attr("class", "gif");
+                gifImage.attr("itemID", x)
+                gifImage.attr("data-state", "still")
+
+                //css for images
+                gifImage.css({
+                    "margin": "2px",
+                     
+                })
+            
+
+                // Prepending the catImage to the images div
+                $("#gifDisplay").prepend(gifImage);
+
+                var results = {
+                    "still-image": imageStill,
+                    "itemID": x,
+                    "image_animate": animatedImage,
+                    "state": "still"
+                }
+                // console.log(results)
+                imageRepo.push(results)
+                $("#user-input").val("")
+
+            };
+            console.log(imageRepo)
+
+
+
+
+
+        })
+
+
+        $(document).on("click", ".gif", function () {
+            var id = $(this).attr("itemID")
+            console.log(id)
+            var state = $(this).attr("data-state")
+            console.log(state)
+            // CODE GOES HERE
+
+            // =============================================
+
+            // STEP THREE: Check if the variable state is equal to 'still',
+            if (state === "still") {
+                $(this).attr("data-state", "animate");
+                $(this).attr("src", $(this).attr("data-animate"))
+            }
+            else {
+                $(this).attr("data-state", "still");
+                $(this).attr("src", $(this).attr("data-still"))
+
+            }
+
+
+        })
+
+
+
+        //get the users input
+        //store the users input
+        //create a button that has the input from the user as the text
+        //append the button to the html
     });
-})
-
-//get the users input
-//store the users input
-//create a button that has the input from the user as the text
-//append the button to the html
+}
